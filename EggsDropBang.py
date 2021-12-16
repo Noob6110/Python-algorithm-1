00b95b106a89ac367873692491c578531bb28513
Ginger <56705896+Noob6110@users.noreply.github.com>
2021-12-16 15:51:16 +0800

    Add files via upload

---
+++ EggsDropBang.py
@@ -0,0 +1,32 @@
+eggsDrop = dict()  # 用于记录重复的子问题的解
+
+
+def dp(K, N):
+    # base case
+    if K == 1:
+        return N
+    if N == 0:
+        return 0
+
+    # 避免重复计算
+    if (K, N) in eggsDrop:
+        return eggsDrop[(K, N)]
+
+    res = float('INF')
+
+    # 穷举所有可能的选择
+    for i in range(1, N + 1):
+        res = min(res,
+                  max(
+                      dp(K, N - i),
+                      dp(K - 1, i - 1)
+                  ) + 1
+                  )
+
+    # 记入字典
+    eggsDrop[(K, N)] = res
+
+    return res
+
+
+print(dp(3, 14))
